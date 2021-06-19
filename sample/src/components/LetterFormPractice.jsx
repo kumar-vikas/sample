819e70e511818dddd16ee22f10bca15db2919916
Nityanand <nityanandojha20@gmail.com>
2021-06-19 10:07:20 +0530

    added letter formation

--- sample/src/components/LetterFormPractice.jsx
+++ sample/src/components/LetterFormPractice.jsx
@@ -6,7 +6,7 @@ import infoBtn from "../images/info-btn.png";
 import penIcon from "../images/pencil-2.png";
 import vidIcon538 from "../images/vidIcon-538.png"
 
-function PreWritingPractice(props){
+function LetterFormPractice(props){
 
 	const [pattern, speed] = props.location.path ? props.location.path.split("$") : ["pat-1", ""];
 
@@ -40,7 +40,7 @@ function PreWritingPractice(props){
 
     return(
         <div className="activity-base" id="act-base-prePrac">
-  	<img alt="" src={pencilImg} class="pencile-image"/>
+  	<img alt="" src={pencilImg} className="pencile-image"/>
   	<div className="dailoug-block-img">
 	 	<img alt="" src="assets/images/dialog-1.png"/>
 	</div>
@@ -55,7 +55,7 @@ function PreWritingPractice(props){
 		</div>
 
 		<div className="bodyTxt">Pre-Writing</div>
-		<div id="vidCont">
+		<div id="letterFormvidCont">
 			<div>
 				<button onClick={fetchVideo}>Slow</button>
 				<span><img src={vidIcon538} alt="" /></span>
@@ -65,10 +65,10 @@ function PreWritingPractice(props){
 		</div>
 
   		
-          <MemoTest canprops={{width:"1090", height:"200", marginTop:108}} className="drawingTool"/>
+          <MemoTest canprops={{width:"550", height:"500", marginTop:-315}} className="drawingTool"/>
   	</div>
   </div>
     )
 }
 
-export default PreWritingPractice;
+export default LetterFormPractice;
--- sample/src/components/precursive.jsx
+++ sample/src/components/precursive.jsx
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import "./activity.css";
 import "./theme.css";
 import pencilImg from "../images/pencil-big.png"
-import animationImg from "../images/animation.png"
+import act1 from "../images/act-1.png";
+import act2 from "../images/act-2.png";
 import infoBtn from "../images/info-btn.png";
 import gameImg from "../images/games.png";
 import printImg from "../images/print-btn.png";
 import { NavLink } from 'react-router-dom';
+import info143 from "../images/info-i143.png"
 
 class Precursive extends Component{
 
@@ -17,7 +19,10 @@ class Precursive extends Component{
         this.state = {
             src:this.props.src,
             visible:'none',
-			name:this.props.location.name
+			name:this.props.location.name,
+
+			help:"If you need help. Click on the information button.",
+			infDiagVis:'none'
         }
 
 		console.log(props)
@@ -62,18 +67,22 @@ class Precursive extends Component{
 		}
 	}
 
+	openDialog=()=>{
+		this.vis = this.vis=="flex" ? "none" : "flex";
+		this.setState({infDiagVis:this.vis})
+	}
+
     render() { 
         return (
 
 			<div className="activity-base">
-				{console.log(this.props.state)}
   	<img alt="" src={pencilImg} className="pencile-image"/>
   	<div className="dailoug-block-img">
 	 	<img alt="" src="assets/images/dialog-1.png"/>
 	</div>
   	<div className="activity-base-inner">
   		<div className="activity-head">
-		  <a className="btn-icon oragnge-btn info-btn">
+		  <a className="btn-icon oragnge-btn info-btn" onClick={this.openDialog}>
 		    <img alt="" src={infoBtn}/>
 		  </a>
 		  <div className="activity-name-block">
@@ -87,58 +96,46 @@ class Precursive extends Component{
 		  </a> */}
 		</div>
   		<div className="activity-folder">
+			  <div className="info-dialog" style={{display:this.state.infDiagVis}}>
+		  		<div>
+		  			<img src={info143} alt="" />
+				  </div>
+				  <div>
+		  				{this.state.help}
+				  </div>
+			  </div>
   			<div className="activity-folder-bg activity-1-wrap">
   				<div className="flex activity-1">
-		  			<div className="activity-assets">
-					  {/* <div className="activity-bubble left">
-						<NavLink to="/videoComp">
-							<div className="activity-icon-wrap">
-							<div className="activity-icon-block">
-								<img alt="" src={animationImg}/>
-							</div>
-							<div className="activity-name-block type4">
-								<p className="activity-name small">Animations</p>
-							</div>
-							</div>
-							</NavLink>
-					  </div> */}
-					  <div className="activity-assets-block">
-					    <div className="activity-bubble circle text">
-					      <div className="">
-					       <img alt="" src="assets/images/act-1.png"/>
-					      </div>
-					    </div>
+		  			<div className="activity-assets">						
+						<div className="activity-assets-block">
+
 						<NavLink to="PreWriting">
-					    <div className="activity-name-block type2">
-					      <p className="activity-name medium">Pre-Writing</p>
-					    </div>
+							<div className="activity-bubble circle text">
+							<div className="">
+							<img alt="" src={act1}/>
+							</div>
+							</div>								
+							<div className="activity-name-block type2">
+							<p className="activity-name medium">Pre-Writing</p>
+							</div>
 						</NavLink>
-					  </div>
-					  {/* <div className="activity-bubble right">
-					  <NavLink to="ActivityComp">
-					    <div className="activity-icon-wrap">
-					      <div className="activity-icon-block">
-					         <img alt="" src={activityImg}/>
-					      </div>
-					      <div className="activity-name-block type4">
-					        <p className="activity-name small">Activities</p>
-					      </div>
-					    </div>
-						</NavLink>
-					  </div> */}
+
+						</div>					  					  
 					</div>
-					<NavLink to="/Warmup">
+
 					<div className="activity-assets-block top">
-						<div className="activity-name-block type3">
-						  <p className="activity-name medium">Warm-Up</p>
-						</div>
-					    <div className="activity-bubble circle text">
-					      <div className="">
-					       <p>Warm-up Video to follow!</p>
-					      </div>
-					    </div>
+						<NavLink to="/Warmup">
+							<div className="activity-name-block type3">
+							<p className="activity-name medium">Warm-Up</p>
+							</div>
+							<div className="activity-bubble circle text">
+							<div className="">
+							<p>Warm-up Video to follow!</p>
+							</div>
+							</div>
+						</NavLink>
 					</div>
-					</NavLink>
+					
 					<div className="activity-assets">
 					  {/* <div className="activity-bubble left">
 					    <div className="activity-icon-wrap">
@@ -150,16 +147,18 @@ class Precursive extends Component{
 					      </div>
 					    </div>
 					  </div> */}
-					  <div className="activity-assets-block">
-					    <div className="activity-bubble circle text">
-					      <div className="">
-					       <img alt="" src="assets/images/act-2.png"/>
-					      </div>
-					    </div>
-					    <div className="activity-name-block type2">
-					      <p className="activity-name medium">Letter Formation</p>
-					    </div>
-					  </div>
+					<div className="activity-assets-block">
+						<NavLink to="/LetterFormation">
+							<div className="activity-bubble circle text">
+								<div className="">
+								<img alt="" src={act2}/>
+								</div>
+							</div>
+							<div className="activity-name-block type2">
+								<p className="activity-name medium">Letter Formation</p>
+							</div>
+						</NavLink>
+					</div>
 					  {/* <div className="activity-bubble right">
 					    <div className="activity-icon-wrap">
 					      <div className="activity-icon-block">
