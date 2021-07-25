30d38b3f398fa6ad91f1b7e10343d69e3387a010
Nityanand <nityanandojha20@gmail.com>
2021-07-25 16:17:36 +0530

    game bg changes 1

--- sample/src/components/context.js
+++ sample/src/components/context.js
@@ -9,16 +9,19 @@ import bgImgB from "../images/BG-images/pre-cur-b-bg-2.png"
 import bgImgB2 from "../images/BG-images/pre-cur-b-bg-5.png"
 import bgImgB3 from "../images/BG-images/pre-cur-b-bg-7.png"
 import bgImgB4 from "../images/BG-images/pre-cur-b-bg-8.png"
+import bgImgB5 from "../images/BG-images/pre-cur-b-bg-11.png"
 
 import bgImgC from "../images/BG-images/pre-cur-c-bg-2.png"
 import bgImgC2 from "../images/BG-images/pre-cur-c-bg-5.png"
 import bgImgC3 from "../images/BG-images/pre-cur-c-bg-7.png"
 import bgImgC4 from "../images/BG-images/pre-cur-c-bg-8.png"
+import bgImgC5 from "../images/BG-images/pre-cur-c-bg-11.png"
 
 import bgImgD from "../images/BG-images/pre-cur-d-bg-2.png"
 import bgImgD2 from "../images/BG-images/pre-cur-d-bg-5.png"
 import bgImgD3 from "../images/BG-images/pre-cur-d-bg-7.png"
 import bgImgD4 from "../images/BG-images/pre-cur-d-bg-8.png"
+import bgImgD5 from "../images/BG-images/pre-cur-d-bg-11.png"
 
 import cursiveA from "../images/BG-images/cursiveBG/cur-a-bg-2.png"
 import cursiveA2 from "../images/BG-images/cursiveBG/cur-a-bg-5.png"
@@ -71,9 +74,9 @@ class MyProvider extends Component {
 
       getImg: {
         PreCursiveA:{a1: bgImgA, a2:bgImgA2, a3:bgImgA3, a4:bgImgA4, a5:bgImgA5},
-        PreCursiveB:{a1: bgImgB, a2:bgImgB2, a3:bgImgB3, a4:bgImgB4},
-        PreCursiveC:{a1: bgImgC, a2:bgImgC2, a3:bgImgC3, a4:bgImgC4},
-        PreCursiveD:{a1: bgImgD, a2:bgImgD2, a3:bgImgD3, a4:bgImgD4},
+        PreCursiveB:{a1: bgImgB, a2:bgImgB2, a3:bgImgB3, a4:bgImgB4, a5:bgImgB5},
+        PreCursiveC:{a1: bgImgC, a2:bgImgC2, a3:bgImgC3, a4:bgImgC4, a5:bgImgC5},
+        PreCursiveD:{a1: bgImgD, a2:bgImgD2, a3:bgImgD3, a4:bgImgD4, a5:bgImgD5},
 
         CursiveA:{a1: cursiveA, a2:cursiveA2, a3:cursiveA3, a4:cursiveA4, a5:cursiveA5},
         CursiveB:{a1: cursiveB, a2:cursiveB2, a3:cursiveB3, a4:cursiveB4, a5:cursiveB5},
